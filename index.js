fbab1c75fcd1e19678a179ecb080215c8294c7d6
+ <+>
2024-03-27 19:47:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Ronald'
+
+const welcomeMessage = `Welcome to Flatbook,${currentUser.slice()}!`
+
+const shortGreeting = `Welcome, ${currentUser.slice()}`
+ 
+console.log(welcomeMessage)
